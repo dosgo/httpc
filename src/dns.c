eaaf6eb24f2b06e1c241ae0098a855688306e519
Konstantin Nazarov <rut.shadowed@gmail.com>
2012-03-09 00:28:36 +0400

    Request/response protocol for host name resolving
    For the dns service to receive requests and replies,
    there should be a (hopefully) simple protocol.
    Since name resolving actually happens in a child
    process, the best way to communicate is through
    text messages. Thus, in current implementation it will
    work as follows:
    
    1. The parent sends a hostname terminated by a newline
       through the request pipe
    2. The child does name resolution
    3. The child replies with the original host name and ipv4
       address separated by a \1 character (usually appears as
       ^A when printed)

--- src/dns.c
+++ src/dns.c
@@ -5,24 +5,72 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
+#include <errno.h>
+#include <sys/epoll.h>
 
 #include "dns.h"
 
+typedef struct dns_request
+{
+    const char *hostname;
+    struct dns_request *next;
+} dns_request;
+
 typedef struct dns_service
 {
-    int data_pipe[2];
+    int request_pipe;
+    int response_pipe;
     int child_pid;
+
+    dns_request *requests;
 } dns_service;
+
+
 
 void
 dns_resolve_loop(dns_service* service)
 {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    FILE *req = fdopen(service->request_pipe, "r");
+
+    int pollfd = epoll_create(1);
+    int nfds = 0;
+
+    if (pollfd == -1) {
+        perror("epoll_create");
+        exit(EXIT_FAILURE);
+    }
+
+    struct epoll_event ev, events[1];
+    ev.events = EPOLLIN;
+    ev.data.fd = service->request_pipe;
+
+    if (epoll_ctl(pollfd, EPOLL_CTL_ADD, service->request_pipe, &ev) == -1) {
+        perror("epoll_ctl: request_pipe");
+        exit(EXIT_FAILURE);
+    }
+
+
     while(1)
     {
-        printf("RESOLVE\n");
+        nfds = epoll_wait(pollfd, events, 1, -1);
+        if (nfds == -1) {
+            perror("epoll_wait");
+            exit(EXIT_FAILURE);
+        }
 
-        sleep(2);
+        while ((read = getline(&line, &len, req)) != -1)
+        {
+            printf("RESOLVE: %s", line);
+
+            free(line);
+            len = 0;
+            line = NULL;
+        }
     }
+
 }
 
 dns_service*
@@ -30,10 +78,17 @@ create_dns_service(void)
 {
     dns_service *service = 0;
     int rc = 0;
+    int request_pipe[2];
+    int response_pipe[2];
 
     service = (dns_service*)malloc(sizeof(dns_service));
 
-    rc = pipe(service->data_pipe);
+    rc = pipe(request_pipe);
+
+    if (rc == -1)
+        goto on_error;
+
+    rc = pipe(response_pipe);
 
     if (rc == -1)
         goto on_error;
@@ -46,14 +101,24 @@ create_dns_service(void)
 
     if (rc == 0) /* child */
     {
-        close(service->data_pipe[0]);
+        close(request_pipe[1]);
+        close(response_pipe[0]);
+
+        service->request_pipe = request_pipe[0];
+        service->response_pipe = response_pipe[1];
 
         dns_resolve_loop(service);
     }
     else /* parent */
     {
-        close(service->data_pipe[1]);
+        close(request_pipe[0]);
+        close(response_pipe[1]);
+
+        service->request_pipe = request_pipe[1];
+        service->response_pipe = response_pipe[0];
+
         service->child_pid = rc;
+        write(service->request_pipe, "foobar\nb\1az\nsdaf\n", strlen("foobar\nb\1az\nsdaf\n"));
     }
 
     goto on_success;
@@ -72,9 +137,11 @@ delete_dns_service(
 )
 {
     int rc;
-    close(service->data_pipe[0]);
+    close(service->request_pipe);
+    close(service->response_pipe);
 
     waitpid(service->child_pid, &rc, 0);
+    printf("DNS STOP\n");
     free(service);
 }
 
